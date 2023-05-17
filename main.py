5a92ccd420515f0432fc28287931904b8963ba95
st4l1nR <stalinramosbw@gmail.com>
2023-05-16 23:18:52 -0500

    first commit

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from urllib.parse import parse_qs
 
 
 def is_iso_date(date_str):
-    pattern = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$'
-    if not bool(re.match(pattern, date_str)):
-        return False
+
+
     try:
+        pattern = r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$'
+        if not bool(re.match(pattern, date_str)):
+            return False
         datetime.strptime(date_str[:-1], '%Y-%m-%dT%H:%M:%S.%f')
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
 
 
@@ -38,14 +40,15 @@ def mongo_query(q):
     query_array = []
 
     for key, value in output_dict.items():
+
         if (is_iso_date(value)):
             value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
         if (is_string_number(value)):
-            print(value)
             value = int(value)
         if key == 's':
             query_array.append({'$text': {'$search': value}})
         elif isinstance(value, list):
+
             if '.' in key:
                 embed_entries = key.split('.')
                 query_array.append({embed_entries[0]: {'$elemMatch': {
@@ -91,7 +94,7 @@ def mongo_query(q):
     return query
 
 
-query_string = 's=A text search&total=1&name!=banned&price[]=10&price[]=50&schedule.start_at^=2023-05-09T15:30:00.000Z&schedule.end_atv=2023-05-09T15:30:00.000Z&schedule.medium_at=2023-05-09T15:30:00.000Z'
+query_string = 's=A text search&total=1&name!=banned&price[]=11&price[]=50&categories*=[dogs,cats]&schedule.start_at^=2023-05-09T15:30:00.000Z&schedule.end_atv=2023-05-09T15:30:00.000Z&schedule.medium_at=2023-05-09T15:30:00.000Z'
 query = mongo_query(query_string)
 print(query) # {'schedule.start_at': {'$gte': datetime.datetime(2023, 5, 9, 15, 30)}, 'schedule.end_at': {'$lte': datetime.datetime(2023, 5, 9, 15, 30)}, 'schedule.medium_at': datetime.datetime(2023, 5, 9, 15, 30), 'number': 1}
 
